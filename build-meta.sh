243ed1c7993eb9c9ab0982af02826602a9360800
Marc Lainez <marc.lainez@gmail.com>
2026-03-24 11:52:20 +0100

    fix(build): generalize build-meta.sh for multiple meta-packages

--- build-meta.sh
+++ build-meta.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
-dpkg-deb --build meta-packages/citronics-lime citronics-lime_1.0_armhf.deb
-echo "Built citronics-lime_1.0_armhf.deb"
+for pkg_dir in meta-packages/*/; do
+  if [ -d "${pkg_dir}DEBIAN" ]; then
+    pkg_name=$(grep '^Package:' "${pkg_dir}DEBIAN/control" | awk '{print $2}')
+    pkg_version=$(grep '^Version:' "${pkg_dir}DEBIAN/control" | awk '{print $2}')
+    pkg_arch=$(grep '^Architecture:' "${pkg_dir}DEBIAN/control" | awk '{print $2}')
+    dpkg-deb --build "$pkg_dir" "${pkg_name}_${pkg_version}_${pkg_arch}.deb"
+    echo "Built ${pkg_name}_${pkg_version}_${pkg_arch}.deb"
+  fi
+done
