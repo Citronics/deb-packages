26efdff048cd6db2a8a1736d2a92be7953f97503
Marc Lainez <marc.lainez@gmail.com>
2026-05-30 12:26:07 +0200

    feat(apt): add arm64 architecture support to repo workflow

--- release.sh
+++ release.sh
@@ -3,3 +3,6 @@
 mkdir -p dists/stable/main/binary-armhf
 dpkg-scanpackages pool/ /dev/null > dists/stable/main/binary-armhf/Packages
 gzip -k -f dists/stable/main/binary-armhf/Packages
+mkdir -p dists/stable/main/binary-arm64
+cp dists/stable/main/binary-armhf/Packages dists/stable/main/binary-arm64/Packages
+gzip -k -f dists/stable/main/binary-arm64/Packages
